08875859b1de255f655b978d35eb5cd954ae76b6
Habtamu Sisay <teshomehabtamu07@gmail.com>
2025-03-05 13:14:58 +0100

    initial commit

---
+++ task3/src/main/java/org/example/task3.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Random;
+public class task3 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int number = rand.nextInt(100) + 1;
+
+        System.out.println("Generated number: " + number);
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+
+    }
+
+}
